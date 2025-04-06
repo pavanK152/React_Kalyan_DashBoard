81ddcc9364605c6e5447f2fc1e9263264822b494
PavanK152 <neikora0245@gmail.com>
2025-04-06 07:37:58 +0530

    Change the path to live in api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000";
+//export const API_URL = "http://localhost:4000";
 
-// const API_URL = "https://backend-nodejs-kalyan.onrender.com"
+const API_URL = "https://backend-nodejs-kalyan.onrender.com";
